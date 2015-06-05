742ae1a509043ce16b88ebf2efe4726d66417ef8
Max Ogden <max@maxogden.com>
2015-06-05 13:19:29 -0700

    peg versions

---
+++ test.js
@@ -0,0 +1,16 @@
+var gasket = require('./')
+var through = require('through2')
+console.log('running pipeline...')
+
+var pipeline = {"example":[{"type":"pipe","command":"echo hello world"},{"type":"pipe","command":"transform-uppercase"}]}
+
+var pipelines = gasket(pipeline)
+var stringifier = through.obj(function (buff, enc, next) {
+  next(null, buff.toString())
+})
+
+pipelines.run('example').pipe(stringifier).pipe(process.stdout)
+
+stringifier.on('finish', function () {
+  console.log('stringify finish')
+})
